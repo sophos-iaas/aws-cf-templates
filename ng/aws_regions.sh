42338295c4b1d8d74c35f4d3a1c2844e6a1b2563
Georg Fleig <georg.fleig@sophos.de>
2016-11-02 09:48:46 +0100

    NUTM-4800 next gen Makefile and helper scripts, WIP

---
+++ ng/aws_regions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Optional arguments: AWS profile list, default: default govcloud
+# Returns all regions accessible by the provided AWS profiles
+
+aws_profiles="default govcloud"
+
+aws_regions() {
+  regions=$(aws ec2 describe-regions --profile $1)
+  if [ ! $? -eq 0 ]; then
+    echo "profile in question: $1" >&2
+  fi
+  echo $(echo $regions | jq -r ".Regions[].RegionName")
+}
+
+if [[ -z "$@" ]]; then
+  aws_profiles="default govcloud"
+else
+  aws_profiles=$@
+fi
+
+all_regions=""
+for profile in $aws_profiles; do
+  all_regions+="$(aws_regions $profile) "
+done
+echo $all_regions
