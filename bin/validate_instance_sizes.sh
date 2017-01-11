05e51bdb7127a852e0263624517bb2b36d4c179c
Sebastian Poehn <sebastian.poehn@sophos.com>
2017-01-11 09:43:34 +0100

    NUTM-6423 Instance Size Validator

---
+++ bin/validate_instance_sizes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This does not make a real difference unless it's linux
+TRY_AMI="ha_byol.ami"
+
+validate_instance(){
+REGION="$1"
+SIZE="$2"
+AMI="$3"
+CMD="../bin/_validate_instance_size.sh $REGION $SIZE $AMI"
+#echo $CMD
+$CMD
+}
+
+cd tmp
+
+for i in * ; do
+  if [ ! -d $i ] ; then
+          continue
+  fi
+  validate_instance $i $(cat $i/default_instance_type.static) $(cat $i/$TRY_AMI | cut -f1)
+  validate_instance $i $(cat $i/larger_instance_type.static) $(cat $i/$TRY_AMI | cut -f1)
+done
+
+cd - > /dev/null
