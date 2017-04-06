745d0673558547c23608f210f5093711a18e366e
Sebastian Poehn <sebastian.poehn@sophos.com>
2017-04-06 13:32:13 +0200

    Add capture group

--- bin/version_parser.sh
+++ bin/version_parser.sh
@@ -6,7 +6,7 @@ if [[ $VERSION =~ ^([0-9])\.([0-9])$ ]]; then
 	major=${BASH_REMATCH[1]}
 	minor=${BASH_REMATCH[2]}
 	lesser_minor=$(($minor-1))
-	VERSION="$major.${lesser_minor}[7-9]|$major.${minor}[0-6]"
+	VERSION="($major.${lesser_minor}[7-9]|$major.${minor}[0-6])"
 fi
 
 echo $VERSION
